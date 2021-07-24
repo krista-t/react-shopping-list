175a7ddc14295c2f9c331438b37936548a1cc3ce
krista-t <krista.tomicic@gmail.com>
2021-07-24 13:50:18 +0200

    list items added

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,27 +1,15 @@
-import { FaTrashAlt } from "react-icons/fa";
+import LineItem from "./LineItem";
 const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li className="item" key={item.id}>
-          <input
-            type="checkbox"
-            //listen to change and call function-we have to pass call into anonymous function
-            onChange={() => handleCheck(item.id)}
-            checked={item.checked}
-          />
-          <label
-            style={item.checked ? { textDecoration: "line-through" } : null}
-            onDoubleClick={() => handleCheck(item.id)}
-          >
-            {item.item}
-          </label>
-          <FaTrashAlt
-            onClick={() => handleDelete(item.id)}
-            role="button"
-            tabIndex="0"
-          ></FaTrashAlt>
-        </li>
+        //LINEITEM IS A COMPONENT
+        <LineItem
+          key={item.id}
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ))}
     </ul>
   );
---
+++ src/LineItem.js
@@ -0,0 +1,25 @@
+import { FaTrashAlt } from "react-icons/fa";
+const LineItem = ({ item, handleCheck, handleDelete }) => (
+  <li className="item" key={item.id}>
+    <input
+      type="checkbox"
+      //listen to change and call function-we have to pass call into anonymous function
+      onChange={() => handleCheck(item.id)}
+      checked={item.checked}
+    />
+    <label
+      style={item.checked ? { textDecoration: "line-through" } : null}
+      onDoubleClick={() => handleCheck(item.id)}
+    >
+      {item.item}
+    </label>
+    <FaTrashAlt
+      onClick={() => handleDelete(item.id)}
+      role="button"
+      tabIndex="0"
+      aria-label={`Delete ${item.item}`}
+    ></FaTrashAlt>
+  </li>
+);
+
+export default LineItem;
